045bfbfd08578c340ed048d5784582c2ff214a9b
Negrea-Andrei <negrea97@yahoo.com>
2024-03-01 22:09:51 +0200

    added cards (need to add slider

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,12 +4,11 @@ const Card = () => {
   return (
     <div className="z-10 bg-white drop-shadow-md overflow-hidden rounded-2xl mr-2  my-4">
       <img className="w-full h-40 object-cover" src={course1} />
-      <div className="p-5">
-        <h1>Test</h1>
-        <div className="flex">
-            <Rating />
-        </div>
+      <div className="p-5 border border-b">
+        <h1 className="py-2">Test</h1>
+        <Rating rating={4} />
       </div>
+      <h3 className="p-5 text-xl">500$</h3>
     </div>
   );
 };
--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -4,6 +4,7 @@ const Courses = () => {
   return (
     <div className='w-full bg-[#E9F8F3B2] py-32'>
       <div className="md:max-w-[1480px] m-auto grid md:grid-cols-2 max-w-[600px]  px-4 md:px-0">
+        {/* Need to add slider*/}
         <Card />
       </div>
     </div>
