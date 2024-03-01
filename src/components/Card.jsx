9afd1adc6b866e639f1152f71d84f7913859a9fa
Negrea-Andrei <negrea97@yahoo.com>
2024-03-01 21:53:45 +0200

    test the star rating

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,13 +1,17 @@
-import course1 from "../assets/course1.jpg"
+import course1 from "../assets/course1.jpg";
+import Rating from "./Rating";
 const Card = () => {
-    return (
-        <div className='z-10 bg-white drop-shadow-md overflow-hidden rounded-2xl mr-2  my-4'>
-            <img className="w-full h-40 object-cover" src={course1} />
-            <div className="p-5">
-                <h1>Test</h1>
-            </div>
+  return (
+    <div className="z-10 bg-white drop-shadow-md overflow-hidden rounded-2xl mr-2  my-4">
+      <img className="w-full h-40 object-cover" src={course1} />
+      <div className="p-5">
+        <h1>Test</h1>
+        <div className="flex">
+            <Rating />
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Card
+export default Card;
